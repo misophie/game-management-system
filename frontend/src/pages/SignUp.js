f73714b959e2e2d704b5111c8c8facfe1ba359cf
Sophie <sophsong@student.ubc.ca>
2023-11-08 10:11:35 -0800

    Copied CSS Components

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,9 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction column;
+    margin-top: 5%;
+    justify-content: center;
+    align-items: center;
+    height: 100%    
+`
+
+const Title = styled.text`
+    font-size: 20px;
+    color: #333;
+    font-weight: bold
+`
+
+const Text = styled.text`
+    font.size: 16px;
+    color: #333;
+    font-weight: bold
+`
+
+const InputTextBox = styled.text`
+    width = 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    outline: none
+`
+
+const SignUpButton = styled.button`
+    background-color: #007bff;
+    color: white; 
+    font-size: 16px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 50%;
+`
 
 export const SignUp = () => {
     return(
-        <div>
-            Sign Up Page
-        </div>
+        <PageContainer>
+            <Title>Sign Up Page</Title>
+        </PageContainer>
     )
 }
